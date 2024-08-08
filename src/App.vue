c8da8be811af89cb6361ceb966ded790f7356884
Evgeniy Shadrin <e.shadrin@aristos.pw>
2024-08-08 06:54:36 +0300

    Добавление компонента TimeLine

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import TimeLine from './components/TimeLine.vue';
 </script>
 
 <template>
-  <div></div>
+  <div class="section">
+    <div class="container">
+      <TimeLine />
+    </div>
+  </div>
 </template>
 
 <style>
+@import 'https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css';
 </style>
---
+++ src/components/TimeLine.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const periods = ['Today', 'This Week', 'This Month'] as const;
+
+type Period = typeof periods[number];
+
+const selectedPeriod = ref<Period>(periods[0]);
+
+function selectPeriod(period: Period) {
+  selectedPeriod.value = period;
+}
+</script>
+
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPeriod }"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </span>
+  </nav>
+</template>
